fa977241a63a9b774ae621cbbdb88578b03f7805
Rasbakimo <bakimocleckley@yahoo.com>
2021-03-25 14:40:06 -0400

    1 more

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -31,7 +31,7 @@ router.get('/:id',async (req, res) => {
   }
 });
 // create new product
-rrouter.post('/', (req, res) => {
+router.post('/', (req, res) => {
   Product.create({
     product_name: req.body.product_name,
     price: req.body.price,
