273fb98112f1336551e1634a52a04d2da649c969
Rasbakimo <bakimocleckley@yahoo.com>
2021-03-27 13:29:31 -0400

    post and delete work

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,8 +7,8 @@ router.get('/',async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.create(req.params.id, {
-      include: [{ model:Product}, {model: ProductTag}]
+    const tagData = await Tag.findAll(req.params.id, {
+      include: [{ model:Product}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -20,8 +20,8 @@ router.get('/:id',async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData= await Tag.create(req.params.id, {
-      include: [{ model:Product}, {model: ProductTag}]
+    const tagData= await Tag.findByPk(req.params.id, {
+      include: [{ model:Product}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -32,7 +32,9 @@ router.get('/:id',async (req, res) => {
 router.post('/',async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Category.create(req.params.id, {
+    const tagData = await Tag.create({
+      id:req.body.id, 
+      tag_name: req.body.tag_name
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -43,7 +45,10 @@ router.post('/',async (req, res) => {
 router.put('/:id',async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Category.create(req.params.id, {
+    const tagData = await Tag.update(req.body.id, {
+      where: {
+        id:req.params.id,
+      },
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -54,7 +59,10 @@ router.put('/:id',async (req, res) => {
 router.delete('/:id',async (req, res) => {
   // delete on tag by its `id` 
   try {
-    const tagData = await Category.create(req.params.id, {
+    const tagData = await Tag.destroy({
+      where: {
+        id:req.params.id,
+      },
     });
     res.status(200).json(tagData);
   } catch (err) {
